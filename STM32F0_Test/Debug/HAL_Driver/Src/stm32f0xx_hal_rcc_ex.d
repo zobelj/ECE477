21cf2edeb6a064a85e6983f545d600309dfee3cd
Joe Zobel <zobelj@purdue.edu>
2022-09-08 12:26:51 -0400

    added STM32F091 workspace

---
+++ STM32F0_Test/Debug/HAL_Driver/Src/stm32f0xx_hal_rcc_ex.d
@@ -0,0 +1,158 @@
+HAL_Driver/Src/stm32f0xx_hal_rcc_ex.o: \
+ ../HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_conf.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/CMSIS/device/stm32f0xx.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/CMSIS/device/stm32f091xc.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/CMSIS/core/core_cm0.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/CMSIS/core/core_cmInstr.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/CMSIS/core/cmsis_gcc.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/CMSIS/core/core_cmFunc.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/CMSIS/device/system_stm32f0xx.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_adc.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_can.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_cec.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_comp.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_crc.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_crc_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_dac.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_dac_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_i2s.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_irda.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_irda_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_iwdg.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_pcd.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_rtc.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_rtc_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_smartcard.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_smartcard_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_smbus.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_spi.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_spi_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_tim.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_tsc.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_usart.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_usart_ex.h \
+ /Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_wwdg.h
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_conf.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/CMSIS/device/stm32f0xx.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/CMSIS/device/stm32f091xc.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/CMSIS/core/core_cm0.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/CMSIS/core/core_cmInstr.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/CMSIS/core/cmsis_gcc.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/CMSIS/core/core_cmFunc.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/CMSIS/device/system_stm32f0xx.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_adc.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_can.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_cec.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_comp.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_crc.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_crc_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_dac.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_dac_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_i2s.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_irda.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_irda_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_iwdg.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_pcd.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_rtc.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_rtc_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_smartcard.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_smartcard_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_smbus.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_spi.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_spi_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_tim.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_tsc.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_usart.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_usart_ex.h:
+
+/Users/joe/Documents/workspace/STM32F0_Test/HAL_Driver/Inc/stm32f0xx_hal_wwdg.h:
